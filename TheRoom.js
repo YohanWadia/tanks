5f12b973dd1addc704d34142f8882c995a5b8a3d
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2023-12-31 17:05:31 +0200

    Copied->Glitch

---
+++ TheRoom.js
@@ -0,0 +1,11 @@
+class TheRoom {
+    constructor(id, room_num) {
+        this.id=id;
+        this.room_num=room_num
+        this.count;
+        this.toBsent;
+        this.ended = true;
+    }
+}
+
+module.exports = TheRoom;
