31dac2efcea8e87b2cb18796992b9bb59bba80be
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-01-05 12:20:24 +0200

    MultiPly Array

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -44,9 +44,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "506a9545aa799a4eb7e56ec54bcbaf72
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "51757276ba536f9baade698231851a50",
-"/": "51757276ba536f9baade698231851a50",
-"main.dart.js": "e341c3f1f6354316ce2e9be19a7ad217",
+"index.html": "b6c1608e37879744f2e997176cb03048",
+"/": "b6c1608e37879744f2e997176cb03048",
+"main.dart.js": "20812852048c5d74d4e63e2d53accfbb",
 "manifest.json": "85f72a84bb189e22ecc65b59a6e88237",
 "version.json": "6317ae4c635d34c40e02bc283b1ab684"};
 // The application shell files that are downloaded before a service worker can
