7b3e87625f283e4320e10b328c9281a4a9bfb815
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-01-11 13:01:40 +0200

    No HitBx paint

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c51e543a86aeb5d97cefc74fdd1eb299
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0ba8e4eb969d9019ca21898e5bd24317",
-"/": "0ba8e4eb969d9019ca21898e5bd24317",
-"main.dart.js": "c35b72b523ffda1a9d35de9e5649cc3b",
+"index.html": "511a9e3f21cb185ec66332737285b07f",
+"/": "511a9e3f21cb185ec66332737285b07f",
+"main.dart.js": "7e78dc15fdfa1b3d26133de9d249a688",
 "manifest.json": "85f72a84bb189e22ecc65b59a6e88237",
 "version.json": "6317ae4c635d34c40e02bc283b1ab684"};
 // The application shell files that are downloaded before a service worker can
